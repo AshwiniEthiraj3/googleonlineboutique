aef89b957c9550706c12170c3285e9877ae00a2d
ashwiniethiraj3 <ashwiniethiraj@gmail.com>
2025-10-22 14:03:40 +0530

    modified checkout-ci

---
+++ src/checkoutservice/main_test.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+    "testing"
+)
+
+// Simple test to verify that Go testing runs correctly
+func TestMainFunction(t *testing.T) {
+    t.Log("Checkoutservice tests executed successfully")
+}
